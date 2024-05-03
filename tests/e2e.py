2a3d2910518c650610bd8e00063aa4f6db20227e
rinsuki <428rinsuki+git@gmail.com>
2024-05-04 00:13:10 +0900

    wait mounter

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,6 +1,7 @@
 import tempfile
 import os
 import subprocess
+import time
 
 with tempfile.TemporaryDirectory() as tmpdir:
     srcdir = os.path.join(tmpdir, 'src')
@@ -14,6 +15,14 @@ with tempfile.TemporaryDirectory() as tmpdir:
     subprocess.run(["./mayakashi.exe", "create", "-i", srcdir, "-o", os.path.join(tmpdir, 'hello'), "-j", "2"]).check_returncode()
     mounter = subprocess.Popen(["./marmounter.exe", "./hello.mar", "mountpoint=" + mountdir, "overlaydir=" + overlaydir])
     try:
+        # first, we need to wait until mounter is ready
+        start_time = time.time()
+        while time.time() - start_time < 60:
+            time.sleep(1)
+            if mounter.poll() is not None:
+                raise Exception("mounter unexpectedly terminated with code " + str(mounter.returncode))
+            if os.path.exists(os.path.join(mountdir, 'test.txt')):
+                break
         with open(os.path.join(mountdir, 'test.txt'), 'r') as f:
             assert f.read() == 'Hello'
     finally:
