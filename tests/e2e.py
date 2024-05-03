4373202a18f2e4a1df7a482a738232c01159c337
rinsuki <428rinsuki+git@gmail.com>
2024-05-04 01:37:03 +0900

    add some test

--- tests/e2e.py
+++ tests/e2e.py
@@ -37,8 +37,26 @@ with tempfile.TemporaryDirectory() as tmpdir:
                 raise Exception("mounter unexpectedly terminated with code " + str(mounter.returncode))
             if os.path.exists(os.path.join(mountdir, 'test.txt')):
                 break
+        
+        print("Test 1 - Read from Archive")
+
         with open(os.path.join(mountdir, 'test.txt'), 'r') as f:
             assert f.read() == 'Hello'
+        assert os.path.exists(os.path.join(overlaydir, 'test.txt')) == False
+
+        print("Test 2 - Create to mounted directory")
+        with open(os.path.join(mountdir, 'test2.txt'), 'w') as f:
+            f.write('Hi')
+        print("Test 2.1 - Exists on overlay directory")
+        assert os.path.exists(os.path.join(overlaydir, 'test2.txt'))
+        print("Test 2.2 - Readable from overlay directory")
+        with open(os.path.join(overlaydir, 'test2.txt'), 'r') as f:
+            assert f.read() == 'Hi'
+        print("Test 2.3 - Readable from mounted directory")
+        with open(os.path.join(mountdir, 'test2.txt'), 'r') as f:
+            assert f.read() == 'Hi'
+
+        print("Test Done!")
     finally:
         mounter.terminate()
     
